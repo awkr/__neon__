8eb5e68d0b0f9516311b434f0ffce1003e63ff50
Zhu Hongjian <zhu.life@gmail.com>
2023-05-17 00:34:04 +0800

    launch & shutdown window

--- main.cc
+++ main.cc
@@ -1,3 +1,39 @@
+#include <GLFW/glfw3.h>
 #include <cstdio>
 
-int main() { printf("hello, stranger.\n"); }
+uint32_t window_width = 800;
+uint32_t window_height = 500;
+
+int main() {
+  printf("hello, stranger.\n");
+
+  if (!glfwInit() || !glfwVulkanSupported()) {
+    return 1;
+  }
+
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+  auto window =
+      glfwCreateWindow(window_width, window_height, "neon", nullptr, nullptr);
+  if (!window) {
+    glfwTerminate();
+    return 1;
+  }
+
+  glfwSetKeyCallback(window, [](GLFWwindow *window, int key, int scancode,
+                                int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+      glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+  });
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
